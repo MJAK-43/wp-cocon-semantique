94c99d3f93038e2c3a605376b39aad34f22187f8
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-04-10 16:09:15 +0200

    Test

--- includes/class-csb-admin.php
+++ includes/class-csb-admin.php
@@ -98,8 +98,8 @@ class CSB_Admin {
     private function process_structure($tree) {
         $generator = new CSB_Generator();
         $generator->generate_full_content($tree);
-        $publisher = new CSB_Publisher();
-        $publisher->publish_structure($tree);
+        //$publisher = new CSB_Publisher();
+        //$publisher->publish_structure($tree);
     }
 
     private function generate_slug($title) {
