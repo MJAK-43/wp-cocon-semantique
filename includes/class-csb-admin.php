ecb3e786d2511f748bc68c02e9f565e9c40db20c
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-04-10 14:44:29 +0200

    Test

--- includes/class-csb-admin.php
+++ includes/class-csb-admin.php
@@ -30,7 +30,7 @@ class CSB_Admin {
         if (!empty($keyword) && !empty($nd) && isset($_POST['submit'])) {
             $generator = new CSB_Generator();
             $structure = $generator->generate_structure($keyword, $nd);
-            //var_dump($structure);
+            var_dump($structure);
         }
 
         // Si l'utilisateur clique sur "Valider et publier"
@@ -136,9 +136,8 @@ class CSB_Admin {
     }
 
     private function render_structure_textarea_html($structure) {
-        echo '////////////////////////////////////////////////////////////////////////////////////////////////////////////';
         if (empty($structure)) {
-           
+            //echo '////////////////////////////////////////////////////////////////////////////////////////////////////////////';
             return;
         }
         //
