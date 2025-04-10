49650665b139d685a8cfff488c9c541cd23b5fd9
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-04-10 16:52:29 +0200

    Test

--- cocon-semantique.php
+++ cocon-semantique.php
@@ -10,19 +10,20 @@ if (!defined('ABSPATH')) exit; // Sécurité
 
 define('CSB_PATH', plugin_dir_path(__FILE__));
 define('CSB_URL', plugin_dir_url(__FILE__));
-echo "DOG";
+
+
 // Inclure les fichiers du plugin
+require_once CSB_PATH . './includes/class-csb-admin.php';
+require_once CSB_PATH . './includes/class-csb-settings.php';
 
-require_once CSB_PATH . 'includes/class-csb-settings.php';
+require_once CSB_PATH . './includes/class-csb-publisher.php';
 
-require_once CSB_PATH . 'includes/class-csb-publisher.php';
+require_once CSB_PATH . './includes/class-csb-generator.php';
 
-require_once CSB_PATH . 'includes/class-csb-generator.php';
 
-require_once CSB_PATH . 'includes/class-csb-admin.php';
-new CSB_Publisher();
-new CSB_Generator();
-new CSB_Settings();
+// new CSB_Publisher();
+// new CSB_Generator();
+// new CSB_Settings();
 new CSB_Admin();
 if (file_exists(CSB_PATH . 'includes/linker.php')) require_once CSB_PATH . 'includes/linker.php';
 
