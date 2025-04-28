52c6d3895359d914bd503ea27609bc1af6a45826
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-04-28 11:04:14 +0200

    test

--- includes/class-csb-admin.php
+++ includes/class-csb-admin.php
@@ -163,10 +163,7 @@ class CSB_Admin {
         $publisher = new CSB_Publisher();
         $linker = new CSB_Linker();
         //$generator = new CSB_Generator();
-        echo "<br>";echo "<br>";
-        print_r("expected_children_count = ");
-        print_r($this->generator->expected_children_count);
-        echo "<br>";echo "<br>";
+        echo '<div class="notice notice-info"><p>expected_children_count = ' . intval($this->generator->expected_children_count) . '</p></div>';
         // Exemple d'utilisation :
         // $file_url = 'https://app.posteria.fr/crons/freepikImageCoconSemantique/chatnoir/chatsurletoitdelamaison'; // Ton lien ici
         // try {
