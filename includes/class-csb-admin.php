3dd6b6ef8f0122e1c03ca83b86b2b15275d9e846
DNicolas7 <152083265+DNicolas7@users.noreply.github.com>
2025-05-05 10:56:48 +0200

    Suppression article de test

--- includes/class-csb-admin.php
+++ includes/class-csb-admin.php
@@ -154,7 +154,7 @@ class CSB_Admin {
         global $wpdb;
         //echo "///////////////////////////////////////";
         $author_id = $wpdb->get_var($wpdb->prepare(
-            "SELECT ID FROM $wpdb->users WHERE user_login = %s",
+            "SELECT ID FROM $wpdb->users WHERE author = %s",
             $author_login
         ));
         
